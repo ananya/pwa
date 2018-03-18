589f19735cfe3404454199c9184088fc08706359
Ryan Davidson <daviryan@aol.com>
2018-03-18 18:33:09 +0000

    Load text input field from local storage

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -23,10 +23,6 @@
     addDialog: document.querySelector('.dialog-container')
   };
 
-  document.ready(()=> {
-    $('#qrCodeMessage').val("Enter your QR Code message or link here");
-    $('#qrcode').qrcode($('#qrCodeMessage').val());
-  });
   /*****************************************************************************
    *
    * Event listeners for UI elements
