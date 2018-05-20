14de27baca24be4e539cfcedb1b1f04f61d8b094
Ananya Agrawal <33188930+ananya-agrawal@users.noreply.github.com>
2018-05-20 14:04:31 +0530

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -70,7 +70,7 @@ self.addEventListener('install', function(e) {
 
 
 self.addEventListener('fetch', function(e) {
-  console.log('[Service Worker] Fetch', e.request.url);
+//   console.log('[Service Worker] Fetch', e.request.url);
   e.respondWith(
     caches.match(e.request).then(function(response) {
       // return response || fetch(e.request);
